f0e7a246e0721e8d26cebc3546708e7a64c2d941
Deary <chickenflakes9@gmail.com>
2019-05-18 02:33:47 +0700

    Change email description

--- contacts/views.py
+++ contacts/views.py
@@ -21,7 +21,7 @@ def contact(request):
       has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
       if has_contacted:
         messages.error(request, 'Ops! You have already made inquiry for this property')
-        return redirect('/listings/'+listing_id)
+        return redirect('/')
 
     # Var matching contacts
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
@@ -30,8 +30,8 @@ def contact(request):
 
     # Send and receives mail using Google SMTP
     send_mail(
-      'Property Listing Inquiry',
-      'There has been an inquiry for' + listing + '. Go check with admin to see more information.', 
+      'Property Listing Inquiry Notification',
+      'Good day! There has been an inquiry for [' + listing + '] Go check with admin to see more information and please contact the seeker to get negotiation. Thank you. Regards, Lovely Admin', 
       'chickenflakes9@gmail.com',
       [realtor_email, 'deary2706@gmail.com'],
       fail_silently=False
